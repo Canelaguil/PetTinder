f98905cce94be21492a17fd9743ed2bf02eb9fab
canelaguil <clara@qlouder.com>
2018-03-12 14:53:58 +0100

    bububu

--- petfind.js
+++ petfind.js
@@ -1,22 +1,18 @@
 $(function () {
  
-document.addEventListener('DOMContentLoaded', bindButtons);
 var apiKey = '8788a1328e6d39c137797752065c325c';
+console.log("grsgsg");
 
-function bindButtons() {
-    document.addEventListener('click', function (event) {
-        event.preventDefault();
-
-
+    $(".btn4").on("click", () => {
         var url = 'http://api.petfinder.com/pet.getRandom';
-
+        var petkind = 'cat';
         $.ajax({
             url: url,
             jsonp: "callback",
             dataType: "jsonp",
             data: {
                 key: apiKey,
-                animal: 'cat',
+                animal: petkind,
                 sex: [0],
                 age: [0],
                 breeds: [0],
@@ -61,169 +57,57 @@ function bindButtons() {
                 }
 
                 var age = response.petfinder.pet.age.$t;
-                var breeds = response.petfinder.pet.breeds.breed.$t;
-
-                var notes = response.petfinder.pet.options.option;
-                for (var i = 0; i < notes.length; i++) {
-                    notes[i] = notes[i].$t;
+                var breed;
+                /*Checks if empty */
+                if (response.petfinder.pet.breeds.breed.$t) {
+                    breed = response.petfinder.pet.breeds.breed.$t;
+                } else {
+                    breed = "Not specified";
                 }
 
 
-                var newImg = document.getElementById("insert_img");
-                newImg.src = img;
 
-                var newName = document.getElementById("insert_name");
+                var newName = document.getElementById("n1");
                 newName.textContent = catName;
 
 
-                var newSex = document.getElementById('gender');
+                var newSex = document.getElementById('g1');
                 newSex.textContent = sex;
 
-                var newAge = document.getElementById('age');
+                var newAge = document.getElementById('a1');
                 newAge.textContent = age;
 
-                var newSize = document.getElementById('size');
+                var newSize = document.getElementById('s1');
                 newSize.textContent = size;
 
-                var newBreed = document.getElementById('breed');
-                newBreed.textContent = breeds;
+                var newBreed = document.getElementById('b1');
+                newBreed.textContent = breed;
 
-                /*   var newNotes = document.getElementById('notes');
-                   if (Boolean(notes)) {
-                       newNotes.textContent = notes;
-                   } else {
-                       newNotes.textContent = "No notes";
-                   } */
 
-                var list = document.createElement("div");
-                console.log(img);
                 var petpic = "url(" + img + ")";
-                console.log(petpic);
                 document.getElementById('petpic').style.backgroundImage = petpic;
-                src.appendChild(newImg);
                 newName.appendChild(newName);
                 newSex.appendChild(newSex);
                 newAge.appendChild(newAge);
                 newSize.appendChild(newSize);
                 newBreed.appendChild(newBreed);
-                newNotes.appendChild(newNotes);
+     
 
             }
+
         });
     })
 
-}
-})
-document.addEventListener('DOMContentLoaded', bindButtons);
-var apiKey = '8788a1328e6d39c137797752065c325c';
-
-function bindButtons(){
-	document.addEventListener('click', function(event){
+  /*  function bindButtons() {
+      
+        document.addEventListener('click', function (event) {
         event.preventDefault();
        
 
-		var url = 'http://api.petfinder.com/pet.getRandom';
-		
-		$.ajax({
-			url: url,
-			jsonp: "callback",
-			dataType: "jsonp",
-			data: {
-				key: apiKey,
-                animal: 'cat',
-                sex: [0],
-                age: [0],
-                breeds: [0],
-                notes: [0],
-				output: 'basic',
-				format: 'json'
-			},
-			
-			success: function( response ) {
-				
-				var catName = response.petfinder.pet.name.$t;
-				var img = response.petfinder.pet.media.photos.photo[2].$t;
-                
-                var size = response.petfinder.pet.size.$t;
-                switch (size){
-                    case "S":
-                        size = "Small";
-                        break;
-                    case "M":
-                        size = "Medium";
-                        break;
-                    case "L":
-                        size = "Large";
-                        break;
-                    case "XL":
-                        size = "Extra Large";
-                        break;
-                    default:
-                        size = size;
-                }
-                
-                var sex = response.petfinder.pet.sex.$t;
-                switch (sex){
-                    case "F":
-                        sex = "Female";
-                        break;
-                    case "M":
-                        sex = "Male";
-                        break;
-                    default:
-                        sex = sex;
-                }
+        
+       
+    })
 
-                var age = response.petfinder.pet.age.$t;
-                var breeds = response.petfinder.pet.breeds.breed.$t;
-                
-                var notes = response.petfinder.pet.options.option;
-                for(var i = 0; i < notes.length; i++){
-                    notes[i] = notes[i].$t;
-                }
-
-			
-                var newImg = document.getElementById("insert_img");
-                newImg.src = img;
-
-                var newName = document.getElementById("insert_name");
-                newName.textContent = catName;
-                
-
-                var newSex = document.getElementById('gender');
-                newSex.textContent = sex;
-                
-                var newAge = document.getElementById('age');
-                newAge.textContent = age;
-
-                var newSize = document.getElementById('size');
-                newSize.textContent = size;
-
-                var newBreed = document.getElementById('breed');
-                newBreed.textContent = breeds;
-            
-             /*   var newNotes = document.getElementById('notes');
-                if (Boolean(notes)) {
-                    newNotes.textContent = notes;
-                } else {
-                    newNotes.textContent = "No notes";
-                } */
-                
-				var list = document.createElement("div");
-                console.log(img);
-                var petpic = "url(" + img + ")";
-                console.log(petpic);
-                document.getElementById('petpic').style.backgroundImage = petpic;
-                src.appendChild(newImg);
-                newName.appendChild(newName);
-                newSex.appendChild(newSex);
-                newAge.appendChild(newAge);
-                newSize.appendChild(newSize);
-                newBreed.appendChild(newBreed);
-                newNotes.appendChild(newNotes);
-				
-			}
-		});
-		})
-
-}
+   }
+    */
+})
